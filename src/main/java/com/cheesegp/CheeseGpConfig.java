01b93de2022b70c6f2668efac8fd02292e605ba6
DuhonTheGuy <53868103+DuhonTheGuy@users.noreply.github.com>
2023-11-08 21:27:57 -0300

    Updated config group

--- src/main/java/com/cheesegp/CheeseGpConfig.java
+++ src/main/java/com/cheesegp/CheeseGpConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("Cheese Coins Config")
 public interface CheeseGpConfig extends Config
 {
 	@ConfigItem(
